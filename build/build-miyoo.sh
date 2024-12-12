927307e03ea46bfc28ec4522aacc9d65af95ddef
stratos <stratosblue@outlook.com>
2024-12-12 21:09:30 +0800

    chore: 重新组织文件结构; 添加armv7构建脚本;

--- build/build-miyoo.sh
+++ build/build-miyoo.sh
@@ -2,7 +2,7 @@
 set -eu
 
 TOOLCHAIN_IMAGE=docker.io/miyoocfw/toolchain-shared-musl:latest
-docker run --rm -i -v $(pwd):/build ${TOOLCHAIN_IMAGE} /bin/bash <<EOF
+docker run --rm -i -v $(pwd)/../src:/build ${TOOLCHAIN_IMAGE} /bin/bash <<EOF
 set -eux
 cd /build
 /opt/miyoo/bin/arm-linux-gcc -std=c11 -Wall -Ofast -march=armv5te -mtune=arm926ej-s -fpic -shared -o gam4980_libretro.so libretro.c
